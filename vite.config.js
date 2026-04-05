a40340a6404cee1e271ef67bd657315b9df8b5f3
Erik Onarheim <erik.onarheim@gmail.com>
2026-04-05 09:21:12 -0500

    chore: add nvmrc + light formatting

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "./",
-build: {
-target: "esnext", //browsers can handle the latest ES features 
-},
-assetsInclude:["**/*.png", "**/*.jpg", "**/*.svg", "**/*.tff"],
+  build: {
+    target: "esnext", //browsers can handle the latest ES features 
+  },
+  assetsInclude: ["**/*.png", "**/*.jpg", "**/*.svg", "**/*.tff"],
 });
